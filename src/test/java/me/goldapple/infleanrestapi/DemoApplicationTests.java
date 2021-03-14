c171f19c1143529b1102b94d7fd6618b530eb91c
kangmeoungoun <kes98202@naver.com>
2021-03-14 20:27:29 +0900

    백기선 인프런 강의 첫 프로젝트 COMMIT

---
+++ src/test/java/me/goldapple/infleanrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.goldapple.infleanrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}

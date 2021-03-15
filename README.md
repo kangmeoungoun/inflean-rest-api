565cae1af563c081aecd4254354dd3c087797404
kangmeoungoun <kes98202@naver.com>
2021-03-15 20:52:20 +0900

    이벤트 API 테스트 클래스 생성

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-### 1.REST API 및 프로젝트 소개
-#### 이벤트 비즈니스 로직
-![image](https://user-images.githubusercontent.com/40969203/111086540-ebd62780-855f-11eb-8df5-982d0e210cdc.png)
+### 2.이벤트 생성 API 개발
+#### 이벤트 API 테스트 클래스 생성
+
+
+```java
+@WebMvcTest 
+//웹과 관련된 테스트 여서 슬라이싱 테스트 타고 부른다.
+//모든 빈들은 등록하는게 아니라 웹과 관련된 것들만 등록
+//근데 이자체를 단위테스트라고 보기 어렵다.
+//MockMvc 웹서버를 뛰우지 않아서 조금 빠르다. 디스패치 서블릿을 만들어야 해서 단위테스트 보다 빠르진 않다.
+
+mockMvc.perform(post("/api/events/"))
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(status().is(201)) //이렇게도 가능 
+```
+
+![image](https://user-images.githubusercontent.com/40969203/111149483-47d89480-85d0-11eb-863e-9382bb2137ee.png)
---
+++ src/test/java/me/goldapple/infleanrestapi/events/EventControllerTest.java
@@ -0,0 +1,28 @@
+package me.goldapple.infleanrestapi.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class EventControllerTest{
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void createEvent() throws Exception{
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+}

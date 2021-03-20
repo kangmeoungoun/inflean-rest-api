1a0399fc735e874a59a3db9e0a4621e3f0c63f35
kangmeoungoun <kes98202@naver.com>
2021-03-20 16:11:38 +0900

    프링 REST Docs 적용

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 ### 3.HATEOAS 와Self-Describtive Message 적용
-#### 스프링 REST Docs 소개
+#### 스프링 REST Docs 적용
 
-##### spring REST Dcos
-    - 레스트 api 문서의 조각을 생성해주는 라이브러리
-    - 우리가 만든 테스트를 실행하면서 요청과응답 응답의 헤더를 이런정보를 사용하여 문서 조각을 만듦
-    - 문서조각을 모아서 REST document 를 만듦 html 로
+![image](https://user-images.githubusercontent.com/40969203/111862169-ebda8b00-8996-11eb-81cd-3d59cef37eb4.png)
--- src/test/java/me/goldapple/infleanrestapi/events/EventControllerTest.java
+++ src/test/java/me/goldapple/infleanrestapi/events/EventControllerTest.java
@@ -2,16 +2,19 @@ package me.goldapple.infleanrestapi.events;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.goldapple.infleanrestapi.common.RestDocsConfiguration;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.properties.PropertyMapping;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,12 +28,15 @@ import java.time.LocalDateTime;
 import java.time.Month;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 class EventControllerTest{
     @Autowired
     MockMvc mockMvc;
@@ -40,13 +46,13 @@ class EventControllerTest{
     @Test
     @DisplayName("정상적으로 이벤트를 생성하는 테스트")
     void createEvent() throws Exception{
-        EventDto event =EventDto.builder()
+        EventDto event = EventDto.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
-                .beginEnrollmentDateTime(LocalDateTime.of(2021, Month.MARCH,15,14,21))
-                .closeEnrollmentDateTime(LocalDateTime.of(2021, Month.MARCH,16,14,21))
-                .beginEventDateTime(LocalDateTime.of(2021, Month.MARCH,17,14,21))
-                .endEventDateTime(LocalDateTime.of(2021, Month.MARCH,18,14,21))
+                .beginEnrollmentDateTime(LocalDateTime.of(2021 , Month.MARCH , 15 , 14 , 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021 , Month.MARCH , 16 , 14 , 21))
+                .beginEventDateTime(LocalDateTime.of(2021 , Month.MARCH , 17 , 14 , 21))
+                .endEventDateTime(LocalDateTime.of(2021 , Month.MARCH , 18 , 14 , 21))
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
@@ -70,22 +76,22 @@ class EventControllerTest{
                 .andExpect(jsonPath("offline").value(true))
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
                 .andExpect(jsonPath("_links.self").exists())
-                //.andExpect(jsonPath("_links.events").exists())
                 .andExpect(jsonPath("_links.query-events").exists())
-                .andExpect(jsonPath("_links.update-event").exists());
+                .andExpect(jsonPath("_links.update-event").exists())
+                .andDo(document("create-event"));
     }
 
     @Test
     @DisplayName("입력받는값이 아닌데 입력값이 들어올때 BadRequest 테스트")
     void createEvent_Bad_Request() throws Exception{
-        Event event =Event.builder()
+        Event event = Event.builder()
                 .id(100)
                 .name("Spring")
                 .description("REST API Development with Spring")
-                .beginEnrollmentDateTime(LocalDateTime.of(2021, Month.MARCH,15,14,21))
-                .closeEnrollmentDateTime(LocalDateTime.of(2021, Month.MARCH,16,14,21))
-                .beginEventDateTime(LocalDateTime.of(2021, Month.MARCH,17,14,21))
-                .endEventDateTime(LocalDateTime.of(2021, Month.MARCH,18,14,21))
+                .beginEnrollmentDateTime(LocalDateTime.of(2021 , Month.MARCH , 15 , 14 , 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021 , Month.MARCH , 16 , 14 , 21))
+                .beginEventDateTime(LocalDateTime.of(2021 , Month.MARCH , 17 , 14 , 21))
+                .endEventDateTime(LocalDateTime.of(2021 , Month.MARCH , 18 , 14 , 21))
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
@@ -110,22 +116,23 @@ class EventControllerTest{
     void createEvent_Bad_Request_Empty_Input() throws Exception{
         EventDto eventDto = EventDto.builder().build();
         this.mockMvc.perform(post("/api/events")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(eventDto)))
+                                     .contentType(MediaType.APPLICATION_JSON)
+                                     .content(objectMapper.writeValueAsString(eventDto)))
                 .andExpect(status().isBadRequest());
     }
+
     @Test
     @DisplayName("입력값이 잘못들어왔을때 에러가 발생하는 테스트")
     void createEvent_Bad_Request_Wrong_Input() throws Exception{
         //등록일자 종료일자가 이벤트 시작일자 종료일자보다 느리게 설정
         //기본가격이 최고 가격보다 높게 설정
-        EventDto eventDto =EventDto.builder()
+        EventDto eventDto = EventDto.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
-                .beginEnrollmentDateTime(LocalDateTime.of(2021, Month.MARCH,26,14,21))
-                .closeEnrollmentDateTime(LocalDateTime.of(2021, Month.MARCH,25,14,21))
-                .beginEventDateTime(LocalDateTime.of(2021, Month.MARCH,24,14,21))
-                .endEventDateTime(LocalDateTime.of(2021, Month.MARCH,23,14,21))
+                .beginEnrollmentDateTime(LocalDateTime.of(2021 , Month.MARCH , 26 , 14 , 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021 , Month.MARCH , 25 , 14 , 21))
+                .beginEventDateTime(LocalDateTime.of(2021 , Month.MARCH , 24 , 14 , 21))
+                .endEventDateTime(LocalDateTime.of(2021 , Month.MARCH , 23 , 14 , 21))
                 .basePrice(10000)
                 .maxPrice(200)
                 .limitOfEnrollment(100)

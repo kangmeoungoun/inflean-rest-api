2eddf4aaac933c70934eb133288cf420ccbe5aaa
kangmeoungoun <kes98202@naver.com>
2021-03-21 21:53:21 +0900

    스프링 시큐리티 기본 설정

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 ### 5.REST API 보안 적용
-#### 예외 테스트
+#### 스프링 시큐리티 기본 설정
+
+##### WebSecurity HttpSecurity 차이
+```java
+public void configure(WebSecurity web) throws Exception{
+        web.ignoring().mvcMatchers("/docs/index.html");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+}
+
+public void configure(HttpSecurity web) throws Exception{}
+
+WebSecurity 걸면 SpringSecurity 필터 타지 않는다.
+HttpSecurity 걸면 SpringSecurity 필터 내에서 걸러진다.
+정적인것들은 처리할떄는 WebSecurity 걸러 주는게 좋다.
+```
+![image](https://user-images.githubusercontent.com/40969203/111905598-d09f7680-8a8f-11eb-9eeb-f8aa7456c797.png)
+
+
---
+++ src/main/java/me/goldapple/infleanrestapi/configs/SecurityConfig.java
@@ -0,0 +1,54 @@
+package me.goldapple.infleanrestapi.configs;
+
+import lombok.RequiredArgsConstructor;
+import me.goldapple.infleanrestapi.accounts.AccountService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private final AccountService accountService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new InMemoryTokenStore();
+    }
+
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(accountService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().mvcMatchers("/docs/index.html");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()); //스프링 부트 기본 정적인 소스 위치
+        /*
+        PathRequest.toStaticResources().atCommonLocations()
+        "/css/**"
+        "/js/**"
+        "/images/**"
+        "/webjars/**"
+        "/favicon.*"
+         */
+    }
+}

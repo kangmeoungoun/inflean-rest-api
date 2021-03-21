45e15143adadb6c1e9bc711a741aeaef5ff86323
kangmeoungoun <kes98202@naver.com>
2021-03-21 20:52:57 +0900

    예외 테스트

--- src/test/java/me/goldapple/infleanrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/goldapple/infleanrestapi/accounts/AccountServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Set;
@@ -38,6 +39,16 @@ class AccountServiceTest {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         //then
         Assertions.assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    @DisplayName("없는 username 으로 사용자 찾기 실패")
+    void findByUsernameFail() throws Exception{
+        //given
+        String username = "random@gmail.com";
+        //when & then
+        UsernameNotFoundException exception = assertThrows(UsernameNotFoundException.class , () -> accountService.loadUserByUsername(username));
+        Assertions.assertThat(exception.getMessage()).contains(username);
     }
 
 }
